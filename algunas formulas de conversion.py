b13615ecdcaf1c74824fe038f194c65de0a2bde7
fredyhack22 <103303538+fredyhack22@users.noreply.github.com>
2022-07-20 01:18:09 -0500

    Add files via upload

---
+++ algunas formulas de conversion.py
@@ -0,0 +1,54 @@
+
+
+a = float(input("digita el numero a convertir a mililitros, poner el dato en litros,  "))
+mililitros = 1000
+
+resultado = (a*mililitros)
+
+
+print(f"convertido a mililitros es :{resultado}")
+
+b = float(input("digita el numero a convertir a metrocubico,poner el dato en litros ,   "))
+metrocubico = 1000
+
+resultado = (b/metrocubico)
+
+print(f"convertido a metrocubico es:{resultado}")
+
+c= float(input("digita el numero a convertir a kilogramos ,poner el dato en gramos ,   "))
+kilogramos = 1000
+
+resultado = (c/kilogramos)
+
+print(f"convertido a kilogramos es:{resultado}")
+
+
+d= float(input("por favor digitar el numero en kilogramos, para convertirlo a gramos"))
+
+resultado=(d*kilogramos)
+
+print(f"convertido a gramos es:{resultado}")
+
+
+e= float(input("por favor digitar la distancia en kilometros, para convertirlo a metros"))
+
+metros=1000
+
+resultado=(e*metros)
+
+print(f"convertido a metros es:{resultado}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
