fdd66cc9f82703aa3f6c3662a9f1eca2370affb2
fredyhack22 <103303538+fredyhack22@users.noreply.github.com>
2022-07-24 22:58:31 -0500

    Add files via upload

---
+++ promedio.py
@@ -0,0 +1,25 @@
+
+def prom(n1,n2,n3,n4,n5):
+    sum = n1+n2+n3+n4+n5
+    prom_valor=sum/5
+    print(prom_valor)
+    return prom_valor
+
+
+miguel=prom(1,2,3,4,3)
+angel=prom(2,3,4,1,2)
+noriega=prom(2,3,1,2,4)
+juan=prom(2,3,4,1,2)
+pablo=prom(2,3,4,2,1)
+
+
+
+print(miguel)
+
+Nota_prom_A29=prom(miguel,angel,noriega,juan,pablo)
+
+print("el promedio del curso es" , Nota_prom_A29)
+
+
+
+
