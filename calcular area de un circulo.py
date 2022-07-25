a6fb28fdf03bfd3c47ddabc63486448a37691024
fredyhack22 <103303538+fredyhack22@users.noreply.github.com>
2022-07-24 23:29:53 -0500

    Add files via upload

---
+++ calcular area de un circulo.py
@@ -0,0 +1,8 @@
+# (A = π r²) formula para calcular el area de un circulo 
+
+def area_circulo(radio):
+    area=radio*radio*3.14
+    return area
+
+
+print("el area de un circulo con r=5 es:", area_circulo(7))
