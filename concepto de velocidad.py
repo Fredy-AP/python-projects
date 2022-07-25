d7906e8d895b46f3fa8db73682d9a84aea352acd
fredyhack22 <103303538+fredyhack22@users.noreply.github.com>
2022-07-25 15:52:45 -0500

    Add files via upload

---
+++ concepto de velocidad.py
@@ -0,0 +1,28 @@
+#la velocidad es el espacio recorrido por un movil en la unidad de tiempo. para obtener la velocidad, v se divide el camino recorrido,  o espacio e, expresado en unidades de longitud , por el tiempo, t , empleado para ello, dado una unidad de tiempo, cm/s o m/s o km/h para un movimiento de velocidad constante, la formula de la velocidad es: e=v.t
+
+
+distancia=int(input("digite la distancia en kilometros   "))
+tiempo= float(input("digite el tiempo en horas  "))
+
+formulaVelocidad= distancia/tiempo
+
+print(formulaVelocidad,"kilometros por  horas ")
+
+distancia=int(input("digite la distancia en kilometros   "))
+velocidad= float(input("digite la velocidad en kilometros por hora  "))
+
+formulaTiempo=distancia/velocidad
+
+print(formulaTiempo,"horas")
+
+tiempo=int(input("digite el tiempo en horas   "))
+velocidad= float(input("digite la velocidad en kilometros por hora  "))
+
+formulaDistancia=velocidad*tiempo
+
+
+print(formulaDistancia,"kilometros")
+
+
+
+
