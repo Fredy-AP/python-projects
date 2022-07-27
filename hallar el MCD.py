c42357ce3217b3e6d03001b4c512adfd0bdb0fdd
fredyhack22 <103303538+fredyhack22@users.noreply.github.com>
2022-07-27 14:50:33 -0500

    Add files via upload

---
+++ hallar el MCD.py
@@ -0,0 +1,16 @@
+def mcd(x,y):
+    mcd=1
+
+    if x%y==0:
+        return y 
+
+    for k in range(int(y/2),0,-1):
+        if x % k ==0 and y % k== 0:
+            mcd=k
+            break
+
+    return mcd
+
+print(mcd(4,2))   
+
+
