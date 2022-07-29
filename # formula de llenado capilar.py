0ac2d1831a5866b40d5a2794a4f9f72e081ba949
fredyhack22 <103303538+fredyhack22@users.noreply.github.com>
2022-07-29 12:12:32 -0500

    Add files via upload

---
+++ # formula de llenado capilar.py
@@ -0,0 +1,16 @@
+# formula de llenado capilar
+
+#clasificacion del llenado capilar segun duracion
+
+tiempo=float(input("digitar el tiempo en segundos"))
+
+if(tiempo>=5):
+    print("choque")
+    if(2<=tiempo<=5):
+        print('retraso')
+        if(tiempo <=2):
+            print("normal")
+
+    
+
+
