c6f353a76dcd5328764d6a2e7161132f0b472636
fredyhack22 <103303538+fredyhack22@users.noreply.github.com>
2022-07-29 12:53:23 -0500

    Add files via upload

---
+++ #Grados Fahrenheit a grados Celsius.py
@@ -0,0 +1,16 @@
+#convertir Grados Centigrados a Farenheit
+
+
+Farenheit=float(input('por favor digitar el numero de Grados Farenheit: '))
+
+formula=(Farenheit-32)*5/9
+formula2=(Farenheit-32)/1.8
+
+print( formula,  'Grados Celsius')
+print( formula2, 'Grados Celsius')
+
+
+
+
+
+
