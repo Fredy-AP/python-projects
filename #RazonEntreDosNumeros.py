97850e6e8e8cf7dbaf68258edb4191b39bac69a2
software development <103303538+fredyhack22@users.noreply.github.com>
2022-09-27 16:35:20 -0500

    Add files via upload

---
+++ #RazonEntreDosNumeros.py
@@ -0,0 +1,28 @@
+# Razones y proporciones
+
+# Razon  entre dos números
+# Recuerda, una razón
+#entre a y b es el
+#cociente entre a y b.
+#Proporción numérica
+# Es una igualdad entre
+#dos razones numéricas
+
+
+#En una clase hay 18 niñas y 9 niños.
+#¿cuál es la razón entre niñas y niños?
+#¿cuál es la razón entre niños y niñas?
+
+Niñas=18
+Niños=9
+
+Formula1=(Niñas/Niños)
+
+print(f"la razon entre niñas y niños es {Formula1}, es decir el numero de niñas es {Formula1} veces el numero de niños")
+
+
+Formula2=(Niños/Niñas)
+
+print(f"la razon entre niñas y niños es {Formula2}, es decir el numero de niños es {Formula2} veces el numero de niñas")
+
+
