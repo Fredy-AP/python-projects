fa3a687648437cae6bfaf224d65aabfb58f7eadb
software development <103303538+fredyhack22@users.noreply.github.com>
2022-09-27 20:46:35 -0500

    Add files via upload

---
+++ ReglaDeTresCompuestaDirecta.py
@@ -0,0 +1,27 @@
+#Regla de Tres COmpuesta Directa
+
+#Regla de tres
+#compuesta directa
+# Se emplea cuando se
+#relacionan tres o mas
+#magnitudes directas
+#conocidas para hallar la
+#desconocida.
+#9 grifos abiertos, durante 10 horas diarias han
+#consumido una cantidad de agua por valor de 20 US$,
+#cual es el valor del agua cuando en los mismos dias se
+#abren 15 grifos durante 12 horas
+
+grifos=9
+Horas=10
+Dollares=20
+grifos1=15
+horas1=12
+
+formula=grifos*Horas
+formula1=grifos1*horas1
+formula3=Dollares*formula1
+formula4=formula3/formula
+print(formula4)
+
+
